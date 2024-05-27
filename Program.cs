3e291588341202380185500e70f6f85c918064b7
johnpatrick2005 <jpgasuncion@csmbataan.ph.education>
2024-05-27 17:08:32 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        string trollComment = "This website is for losers LOL!";
+        string cleanedComment = RemoveVowels(trollComment);
+        Console.WriteLine(cleanedComment);
+        Console.ReadKey(); 
+    }
+
+    static string RemoveVowels(string input)
+    {
+        StringBuilder result = new StringBuilder();
+
+        foreach (char c in input)
+        {
+            if (!IsVowel(c))
+            {
+                result.Append(c);
+            }
+        }
+
+        return result.ToString();
+    }
+
+    static bool IsVowel(char c)
+    {
+        return "aeiouAEIOU".IndexOf(c) != -1;
+    }
+}
